4a0bd8ff75a36c2ab1096e339478ac566ef0544f
Aaron Drinkwater <aarond@element78.co.uk>
2023-01-21 13:03:10 +0000

    Add project files.

---
+++ RecipeBlog/Components/RecipesViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipeBlog.Models.Generated;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace RecipeBlog.Components
+{
+    public class RecipesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(IPublishedContent recipesFolder, int amount)
+        {
+            if (recipesFolder == null) { return View(new List<RecipePage>()); }
+
+            var recipes = recipesFolder.Children
+                .Where(c => c.IsDocumentType(RecipePage.ModelTypeAlias))
+                .Take(amount)
+                .Cast<RecipePage>()
+                .ToList();
+
+            return View(recipes);
+        }
+    }
+}
---
+++ RecipeBlog/Models/ViewModels/HeadMeta.cs
@@ -0,0 +1,11 @@
+﻿namespace RecipeBlog.Models.ViewModels
+{
+    public class HeadMeta
+    {
+        public string PageTitle { get; set; }
+        public string WebsiteName { get; set; }
+        public string SeoDescription { get; set; }
+        public string AbsoluteUrl { get; set; }
+        public UmbracoImage SocialShareImage { get; set; }
+    }
+}
