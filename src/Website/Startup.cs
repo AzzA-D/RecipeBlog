74e84ee3d55564d4f8e1eaac547d0324b3617643
Aaron Drinkwater <aarond@element78.co.uk>
2023-02-04 17:00:19 +0000

    Azure Blob Storage setup

--- src/Website/Startup.cs
+++ src/Website/Startup.cs
@@ -33,6 +33,7 @@ namespace Website
                 .AddBackOffice()
                 .AddWebsite()
                 .AddComposers()
+                .AddAzureBlobMediaFileSystem()
                 .Build();
         }
 
